0c6739fe2d4920b418f862b223b5376c9352d381
chouaiech1988 <chouaiech@hotmail.fr>
2012-08-06 19:59:44 +0200

    second commit

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,22 @@
 package controllers;
 
+import org.codehaus.jackson.JsonNode;
+
 import play.*;
 import play.mvc.*;
+import play.mvc.Http.RequestBody;
 
 import views.html.*;
 
 public class Application extends Controller {
   
   public static Result index() {
-    return ok(index.render("Your new application is ready."));
+	  RequestBody body = request().body();
+	  JsonNode data=body.asJson();
+	  String params=data.get("params").asText();
+	  String event=data.get("event").asText();
+
+    return ok("les params sont : "+params+"l evenement est: "+event);
   }
   
 }
